52993c38ce764589f50237d95b3fd0d49fa7e84f
Alex Jablonski <2461496+ajablonski@users.noreply.github.com>
2022-08-06 20:54:43 -0500

    Add realtime scheduling topic and scheduled job and rename static topic and job

--- terraform/main.tf
+++ terraform/main.tf
@@ -66,19 +66,35 @@ resource "google_storage_bucket" "terraform_state" {
   }
 }
 
-resource "google_pubsub_topic" "scheduling_topic" {
-  name                       = "fetch-gtfs-data-triggers"
+resource "google_pubsub_topic" "static_scheduling_topic" {
+  name                       = "fetch-static-gtfs-data-triggers"
   message_retention_duration = "86400s"
 }
 
-resource "google_cloud_scheduler_job" "fetch_gtfs_data_trigger" {
-  name      = "fetch-gtfs-data-trigger"
+resource "google_pubsub_topic" "realtime_scheduling_topic" {
+  name                       = "fetch-realtime-gtfs-data-triggers"
+  message_retention_duration = "86400s"
+}
+
+resource "google_cloud_scheduler_job" "fetch_static_gtfs_data_trigger" {
+  name      = "fetch-static-gtfs-data-trigger"
   schedule  = "0 0 * * *"
   time_zone = "America/Chicago"
 
   pubsub_target {
-    topic_name = google_pubsub_topic.scheduling_topic.id
-    data       = base64encode(jsonencode({ "trigger" = "fetch-gtfs-data" }))
+    topic_name = google_pubsub_topic.static_scheduling_topic.id
+    data       = base64encode(jsonencode({ "trigger" = "fetch-static-gtfs-data" }))
+  }
+}
+
+resource "google_cloud_scheduler_job" "fetch_realtime_gtfs_data_trigger" {
+  name      = "fetch-realtime-gtfs-data-trigger"
+  schedule  = "* * * * *"
+  time_zone = "America/Chicago"
+
+  pubsub_target {
+    topic_name = google_pubsub_topic.realtime_scheduling_topic.id
+    data       = base64encode(jsonencode({ "trigger" = "fetch-realtime-gtfs-data" }))
   }
 }
 
