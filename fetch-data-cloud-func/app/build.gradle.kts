4c06b08376e177b1ba27bafe2e22236a85e0f305
Alex Jablonski <git@ajablonski.dev>
2025-01-10 10:19:47 -0600

    Upgrade shadow Gradle plugin

--- fetch-data-cloud-func/app/build.gradle.kts
+++ fetch-data-cloud-func/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     kotlin("jvm") version "2.1.0"
     kotlin("plugin.serialization") version "2.1.0"
     id("com.github.ben-manes.versions") version "0.51.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "8.3.5"
 }
 
 repositories {
