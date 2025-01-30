8ef6f5d4e92737cdcf2f5a6cca876b9acc61721e
Alex Jablonski <2461496+ajablonski@users.noreply.github.com>
2025-01-30 15:25:26 -0600

    Update build.gradle.kts
    Upgrade serialization plugin in concert with Kotlin upgrade

--- fetch-data-cloud-func/app/build.gradle.kts
+++ fetch-data-cloud-func/app/build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
     kotlin("jvm") version "2.1.10"
-    kotlin("plugin.serialization") version "2.1.0"
+    kotlin("plugin.serialization") version "2.1.10"
     id("com.github.ben-manes.versions") version "0.52.0"
     id("com.gradleup.shadow") version "8.3.5"
 }
